e5e4637986226ccc73294398311a2303552b4d09
FShinoda <fernanda.shinoda@hotmail.com>
2024-06-24 18:04:07 -0300

    feat: initial webdriver settings

---
+++ scrapingTests.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+service = Service(executable_path='./chromedriver.exe')
+options = webdriver.ChromeOptions()
+driver = webdriver.Chrome(service=service, options=options)
+
+driver.get("https://www.twitter.com")
+
+driver.quit()
